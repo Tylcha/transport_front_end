7fd64d4fcd25788979db3b472610ffcae39ebb4a
DALTONLAR\SEZAİ <a>
2024-09-30 15:59:41 +0300

    +add login post request

---
+++ transportfront/app/Login/page.tsx
@@ -0,0 +1,75 @@
+"use client"
+import React, { useState } from 'react';
+import Image from 'next/image';
+import logo from '@/puclic/logo.svg';
+import Header from '@/components/Header';
+
+
+import axios from 'axios';
+
+function login() {
+    const [tc, setTc] = useState('');
+    const [password, setPassword] = useState('');
+
+
+   
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault(); // Formun varsayılan davranışını engelliyoruz
+
+        try {
+            const response = await axios.post('http://localhost:3000/users/login', {
+                tc,
+                password
+            });
+            console.log('Login successful:', response.data);
+        } catch (error) {
+            console.error('Error during login:', error);
+        }
+    };
+    return (
+        <div>
+            <Header />
+            <section className="bg-gray-50 dark:bg-gray-900 mt-26">
+                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                    <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                        <Image className="w-8 h-8 mr-2" src={logo} alt="logo" width={200} height={200} /> {/* Genişlik ve yükseklik ekledik */}
+                    </a>
+                    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                Hesabiniza Giris Yapin
+                            </h1>
+                            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit} action="#">
+                                <div>
+                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kullanici Adi</label>
+                                    <input type="text" name="tc" id="tc" value={tc} onChange={(e) => setTc(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="kullanici adi" required />
+                                </div>
+                                <div>
+                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sifre</label>
+                                    <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                </div>
+                                <div className="flex items-center justify-between">
+                                    <div className="flex items-start">
+                                        <div className="flex items-center h-5">
+                                            <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" />
+                                        </div>
+                                        <div className="ml-3 text-sm">
+                                            <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Beni Hatirla</label>
+                                        </div>
+                                    </div>
+                                    <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Sifremi Unuttum?</a>
+                                </div>
+                                <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Giris Yap</button>
+                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                    Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                                </p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default login;
--- transportfront/components/Header.tsx
+++ transportfront/components/Header.tsx
@@ -1,6 +1,8 @@
 import next from "next/link";
 import Image from "next/image";
 import logo from '../puclic/logo.svg'
+import link from 'next/link'
+import Link from "next/link";
 
 export default function Header() {
   return (
@@ -11,10 +13,11 @@ export default function Header() {
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo"> */}
           <Image src={logo} alt="logo" ></Image>
-            {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span> */}
+          {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span> */}
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Giriş Yap</button>
+          <Link href="/Login">
+            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Giriş Yap</button></Link>
           <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
