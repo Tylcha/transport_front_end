d7e09275735f76b87996cf95ea81eaa0f97b28a6
DALTONLAR\SEZAİ <a>
2024-10-04 17:11:18 +0300

    +add bug fix

--- transportfront/app/Login/page.tsx
+++ transportfront/app/Login/page.tsx
@@ -8,7 +8,7 @@ import { useRouter } from 'next/navigation'
 import axios from 'axios';
 
 function Login() {
-    const [tc, setTc] = useState('');
+    const [user_name, setUser_name] = useState('');
     const [password, setPassword] = useState('');
     const [errorMessage, setErrorMessage] = useState('') //hata yonetimi
     const [mounted, setMounted] = useState(false); // Mount kontrolü
@@ -23,8 +23,8 @@ function Login() {
         e.preventDefault(); // Formun varsayılan davranışını engelliyoruz
 
         try {
-            const response = await axios.post('http://localhost:3000/users/login', {
-                tc,
+            const response = await axios.post('http://localhost:3000/auth/login', {
+                user_name,
                 password
             });
             // Eğer giriş başarılıysa token'ı kaydediyoruz
@@ -39,7 +39,7 @@ function Login() {
         } catch (error) {
             console.error('Error during login:', error);
             // Eğer giriş başarısızsa kullanıcıya hata mesajı gösterebilirsin
-            setErrorMessage('TC veya şifre hatalı. Lütfen tekrar deneyin.');
+            setErrorMessage('Kullanici adi veya sifre hatali');
         }
     };
     return (
@@ -58,15 +58,16 @@ function Login() {
                             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit} action="#">
                                 <div>
                                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kullanici Adi</label>
-                                    <input type="text" name="tc" id="tc" value={tc} onChange={(e) => setTc(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="kullanici adi" required />
+                                    <input type="text" name="tc" id="tc" value={user_name} onChange={(e) => setUser_name(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="kullanici adi" required />
                                 </div>
                                 <div>
                                     <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sifre</label>
                                     <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                    <label htmlFor="hata" className='block mb-2 mt-4 text-sm font-medium text-red-600 dark:text-white'>{errorMessage}</label>
                                 </div>
                                 <div className="flex items-center justify-between">
                                     <div className="flex items-start">
-                                        <div className="flex items-center h-5">
+                                        <div className="flex items-center h-5"> 
                                             <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" />
                                         </div>
                                         <div className="ml-3 text-sm">
@@ -77,7 +78,7 @@ function Login() {
                                 </div>
                                 <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Giris Yap</button>
                                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                                    Giris yapamiyorsaniz yoneticiyle iletisime gecin <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">iletisim</a>
                                 </p>
                             </form>
                         </div>
--- transportfront/app/adminDashboard/page.tsx
+++ transportfront/app/adminDashboard/page.tsx
@@ -7,13 +7,19 @@ import logo from '@/public/logo.svg';
 
 import axios from 'axios';
 
+interface User {
+    id: number;
+    user_name: string;
+    full_name: string;
+    position: string[];
+}
+
 function Page() {
-    const [userData, setUserData] = useState([]); // useState kullanımı
+    const [userData, setUserData] = useState<User[]>([]); // useState kullanımı
     const [search, setSearch] = useState('');
 
-    const handleChange = async(e: React.ChangeEvent<HTMLInputElement>) =>{
-        const input_value = e.target.value
-        setSearch(input_value)
+    const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value)
     }
     useEffect(() => {
         const fetchData = async () => {
@@ -32,6 +38,17 @@ function Page() {
         e.preventDefault(); // Formun varsayılan davranışını engelliyoruz
         // Form gönderme işlemleri burada yapılacak
     };
+
+    /*filteredUsers: Bu, arama sonuçlarını gösteren listeyi oluşturur. userData üzerinden geçerek her user nesnesini kontrol eder. Eğer tc (TC kimlik numarası) ya da position (pozisyon) arama terimi ile eşleşiyorsa (includes ile), o kullanıcı listeye eklenir.
+    toLowerCase(): Arama işlemini büyük/küçük harf duyarsız hale getirir.
+    join(','): Kullanıcı birden fazla pozisyonda çalışabiliyorsa, pozisyonlarını virgülle birleştirir ve string haline getirir. */
+
+    const filterUser = userData.filter((user) => {
+        return user.full_name.toLowerCase().includes(search.toLowerCase()) || user.position.join(',').toLowerCase().includes(search.toLowerCase());
+    })
+    console.log(userData);
+
+
     return (
         <div>
             <Header />
@@ -72,7 +89,7 @@ function Page() {
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                             </svg>
                         </div>
-                        <input type="text" id="table-search-users" onChange={handleChange} className="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for users" />
+                        <input type="text" id="table-search-users" value={search} onChange={handleChange} className="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for users" />
                     </div>
                 </div>
                 <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -99,34 +116,34 @@ function Page() {
                         </tr>
                     </thead>
                     <tbody>
-                        {userData.map((user) =>(
-                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={user.tc}>
-                            <td className="w-4 p-4">
-                                <div className="flex items-center">
-                                    <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                                    <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
-                                </div>
-                            </td>
-                            <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-                                <Image className="w-10 h-10 rounded-full" src={logo} alt="Jese image" />
-                                <div className="ps-3">
-                                    <div className="text-base font-semibold">{user.tc}</div>
-                                    <div className="font-normal text-gray-500">neil.sims@flowbite.com</div>
-                                </div>
-                            </th>
-                            <td className="px-6 py-4">
-                                {user.position.join(',')}
-                            </td>
-                            <td className="px-6 py-4">
-                                <div className="flex items-center">
-                                    <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> Online
-                                </div>
-                            </td>
-                            <td className="px-6 py-4">
-                                <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</a>
-                            </td>
-                        </tr>
-                    ))}
+                        {filterUser.map((user) => (
+                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={user.id}>
+                                <td className="w-4 p-4">
+                                    <div className="flex items-center">
+                                        <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                        <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
+                                    </div>
+                                </td>
+                                <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                                    <Image className="w-10 h-10 rounded-full" src={logo} alt="Jese image" />
+                                    <div className="ps-3">
+                                        <div className="text-base font-semibold">{user.full_name}</div>
+                                        <div className="font-normal text-gray-500">neil.sims@flowbite.com</div>
+                                    </div>
+                                </th>
+                                <td className="px-6 py-4">
+                                    {user.position.join(',')}
+                                </td>
+                                <td className="px-6 py-4">
+                                    <div className="flex items-center">
+                                        <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> Online
+                                    </div>
+                                </td>
+                                <td className="px-6 py-4">
+                                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</a>
+                                </td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
